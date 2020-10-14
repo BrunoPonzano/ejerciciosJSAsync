485a2587bad294121eb7c3ef5cdf355175259645
BrunoPonzano <70296006+BrunoPonzano@users.noreply.github.com>
2020-10-14 15:09:12 -0300

    Update app.js

--- ejercicio_1/app.js
+++ ejercicio_1/app.js
@@ -3,9 +3,13 @@ const obtenerChiste = require("./library");
 
 // Codigo funcion callback
 
+function funcionCallback(joke){
+  
+  console.log(joke[0]);
 
+}
 
 // Fin codigo
 
 // Usar la funcion obtenerChiste(funcionCallback) en donde funcionCallback es una función que recibe el objeto chiste extraido
-obtenerChiste();
+obtenerChiste(funcionCallback);
