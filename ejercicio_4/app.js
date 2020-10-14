eff1ffb0f3ba4bbda5ee13392e99d242e53d098d
BrunoPonzano <70296006+BrunoPonzano@users.noreply.github.com>
2020-10-14 15:39:39 -0300

    Update app.js

--- ejercicio_4/app.js
+++ ejercicio_4/app.js
@@ -1,5 +1,13 @@
 
-const obtenerPokemon = require("./library");
+const obtenerChiste = require("./library");
 
-// Usar la funcion obtenerChiste(nombrePokemon) la cual recibe como string un nombre de pokemon y devuelve la promesa de traer el objeto pokemon extraido
-obtenerPokemon();
+
+obtenerChiste('pidgeot').then((pokemon)=>{
+console.log("Nombre: "+ pokemon.name);
+return pokemon;
+
+}).then((pokemon)=>{
+    console.log("Habilidad: "+pokemon.abilities[0].ability.name);
+}).catch((err) =>{
+    console.log(err);
+});
