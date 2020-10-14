f776391a37258408eb384a35d2ffacf202d55380
BrunoPonzano <70296006+BrunoPonzano@users.noreply.github.com>
2020-10-14 15:31:51 -0300

    Update app.js

--- ejercicio_3/app.js
+++ ejercicio_3/app.js
@@ -1,11 +1,12 @@
 
+
 const obtenerPokemon = require("./library");
 
 
 
 function funcionCallback(pokemon){
     console.log('Nombre: ' + pokemon.name);
-    console.log('Habilidad: '+pokemon.abilities[0].ability.name);
+    console.log('Habilidad: '+pokemon.abilities[0].ability.name +" & "+  pokemon.abilities[1 ].ability.name);
     console.log('Altura: '+pokemon.height);
     console.log()
 }
